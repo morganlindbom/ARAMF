05576f8c7a9f0c6cf6d441a5673fc7cdf76673d1
Morgan Lindbom <155242531+morganlindbom@users.noreply.github.com>
2026-09-12 03:30:49 +0200

    Use second display for ARAMF startup

--- src/main.cpp
+++ src/main.cpp
@@ -8,26 +8,35 @@
 #include <QCoreApplication>
 #include <QTextStream>
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-    if (argc > 1 && (QString::fromLocal8Bit(argv[1]) == QStringLiteral("--help") || QString::fromLocal8Bit(argv[1]) == QStringLiteral("memory") || QString::fromLocal8Bit(argv[1]) == QStringLiteral("improvement") || QString::fromLocal8Bit(argv[1]) == QStringLiteral("project")))
+    if (argc > 1)
     {
-        QCoreApplication app(argc, argv);
-        QTextStream output(stdout);
-        QTextStream error(stderr);
-        QStringList arguments;
-        for (int index = 1; index < argc; ++index)
-            arguments.append(QString::fromLocal8Bit(argv[index]));
-        if (arguments.value(0) == QStringLiteral("project"))
-            return runProjectRootRebindCommand(arguments, output, error);
-        return runMemoryCommand(arguments, output, error);
+        const QString firstArgument = QString::fromLocal8Bit(argv[1]);
+        if (firstArgument == QStringLiteral("--help") ||
+            firstArgument == QStringLiteral("memory") ||
+            firstArgument == QStringLiteral("improvement") ||
+            firstArgument == QStringLiteral("project"))
+        {
+            QCoreApplication app(argc, argv);
+            QTextStream output(stdout);
+            QTextStream error(stderr);
+
+            QStringList arguments;
+            for (int index = 1; index < argc; ++index)
+                arguments.append(QString::fromLocal8Bit(argv[index]));
+
+            if (arguments.value(0) == QStringLiteral("project"))
+                return runProjectRootRebindCommand(arguments, output, error);
+
+            return runMemoryCommand(arguments, output, error);
+        }
     }
 
     // Starts the ARAMF desktop application.
-
     QApplication app(argc, argv);
 
-    constexpr int screen = 2;
+    constexpr int screen = 1;
     constexpr int windowWidth = 1000;
     constexpr int windowHeight = 600;
 
